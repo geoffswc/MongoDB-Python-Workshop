a55c5bb2cb09ff8f339a1c654974f38fa6f87d86
“Geoffrey <geoffreyboushey@gmail.com>
2020-09-01 16:16:20 -0700

    remove output code

--- data/load_into_mongo.sh
+++ data/load_into_mongo.sh
@@ -1,4 +1,4 @@
 for f in *.json
 do
-	mongoimport --uri "mongodb+srv://covid-19-admin:covid-19-password@python-mongodb-workshop.unmjr.gcp.mongodb.net/covid-19" --collection noncomm-subset --file=$f
+	mongoimport --uri "mongodb+srv://user_naame:user_password@atlas_mongodb_uri/covid-19" --collection noncomm-subset --file=$f
 done
