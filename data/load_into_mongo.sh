d100553ef7f9949778f6cc6051884baf1132ccb5
“Geoffrey <geoffreyboushey@gmail.com>
2020-09-01 16:06:53 -0700

    split cord workbooks into atlas and local hosted

--- data/load_into_mongo.sh
+++ data/load_into_mongo.sh
@@ -1,4 +1,4 @@
 for f in *.json
 do
-	mongoimport --db=covid-noncomm-use-subset --collection=pmc_content --file=$f
+	mongoimport --uri "mongodb+srv://covid-19-admin:covid-19-password@python-mongodb-workshop.unmjr.gcp.mongodb.net/covid-19" --collection noncomm-subset --file=$f
 done
